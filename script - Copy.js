e8f8ba404bf26069a5b1d0821339bc7fdc413c79
Mmil09 <Mmil09@gmail.com>
2013-10-19 17:50:56 -0400

    A lot of changes

---
+++ script - Copy.js
@@ -0,0 +1,45 @@
+
+/* 
+
+var anwswer = Math.floor((Math.random()*100)+1);
+
+
+var userInput = prompt("Please enter a number:");
+
+while (userInput != anwswer) {
+	userInput = prompt("Try again:" + (anwswer - userInput));
+}
+
+var congrats = alert("Nice work!");
+
+*/
+
+$(document).ready(function() {
+
+
+var fsdf = alert("test!!");
+$('div').html("<h1>fsfsdfsdfd</h1>"); 
+
+});
+
+
+
+
+
+
+/* $(document).ready(function() {
+    $('button').click(function() {
+    	var toAdd = $("input[name=message]").val();
+        $('#messages').append("<p>"+toAdd+"</p>");
+    });
+});
+
+var anwswer = Math.floor((Math.random()*100)+1);
+
+function function1() {
+var userInput = document.getElementById('userInput').value;
+var submitButton = document.getElementById.("submitButton");
+
+
+var congrats = alert(userInput);
+}
--- script.js
+++ script.js
@@ -1,17 +1,71 @@
-/* 
-
-This is the basic javascript version 
-
-var anwswer = Math.floor((Math.random()*100)+1);
+$(document).ready(function() {
 
 
-var userInput = prompt("Please enter a number:");
 
-while (userInput != anwswer) {
-	userInput = prompt("Try again:" + (anwswer - userInput));
-}
 
-var congrats = alert("Nice work!");
+	var answer = Math.floor((Math.random()*100)+1);
+	var feedback;
+	var answers = 0;
+	
+	
 
-*/
+	var checkAnswer = function() {
 
+	var userIpt = $('#userInput').val();	
+	var userInput = parseInt(userIpt, 10);
+
+	if(isNaN(userInput)) {
+		alert("Nice try, but your answer must be an number");
+	}
+
+	else if (userInput < 1 || userInput > 100) {
+		alert("must be between 1 and 100")
+	}
+
+	else if (userInput > answer) {
+		alert("too hot");
+	}
+
+	else if (userInput < answer) {
+		alert("too cold " + answer);
+	}
+
+	else if (userInput == answer) {
+		alert("right on");
+	}
+
+	answers++;
+	$('#guesses').append(userInput + " ");
+	$('#gradient').html(answers);
+	$('#userInput').val("");
+	$('#userInput').focus();
+	}
+
+	
+	$('#userInput').focus(); /*direct the focus to the number input area */
+
+	
+	$('#feedback').replaceWith("Guess a number between 1 and 100...");
+	$('#feedback').slideDown(5000);
+
+
+	 $('#submit').click(checkAnswer);
+	 $('#userInput').keyup(function(e) {
+	 		if (e.keyCode == 13) {
+	 			checkAnswer();
+	 		}
+	 });
+
+	 $('#reset').click(function() {
+	 	answer = Math.floor((Math.random()*100)+1);
+		feedback = "Okay - new game";
+		answers = 0;
+		$('#guesses').empty();
+		$('#gradient').html(answers);
+		$('#userInput').val("");
+		$('#userInput').focus();
+
+	 });
+
+
+});
